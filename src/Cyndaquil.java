7297a2d854c1177c5a2bddf39302f7909f6cbf2a
martib007 <martib007@gmail.com>
2016-12-14 19:16:26 -0800

    additional classes
    created classes for the two other pokemon

---
+++ src/Cyndaquil.java
@@ -0,0 +1,11 @@
+public class Cyndaquil {
+	public static void main(String[] args) {
+		//stats
+		int hp = 39;
+		int atk = 52;
+		int def	= 43;
+		int spAtk = 60;
+		int spDef = 50;
+		int speed = 65;
+	}
+}
