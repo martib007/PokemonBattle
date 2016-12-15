1092e2d4b14a0442686b3143ce294f5fe1ab703d
martib007 <martib007@gmail.com>
2016-12-14 19:21:47 -0800

    p
    p

---
+++ src/Mudkip.java
@@ -0,0 +1,11 @@
+public class Mudkip {
+	public static void main(String[] args) {
+		//stats
+		int hp = 50;
+		int atk = 70;
+		int def	= 50;
+		int spAtk = 50;
+		int spDef = 50;
+		int speed = 40;
+	}
+}
